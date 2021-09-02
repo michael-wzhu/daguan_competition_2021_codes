f495e66b7abe33c1d98affcda2e2f2fb0a5176ce
Administrator <michael_wzhu91@163.com>
2021-09-02 18:28:50 +0800

    0829

--- src/bert_models/models/classifier.py
+++ src/bert_models/models/classifier.py
@@ -14,3 +14,31 @@ class Classifier(nn.Module):
         x = self.dropout(x)   # [batch_size, hidden_dim]
 
         return self.linear(x)
+
+
+class MultiSampleClassifier(nn.Module):
+    def __init__(self, args, input_dim=128, num_labels=2):
+        super(MultiSampleClassifier, self).__init__()
+        self.args = args
+
+        self.linear = nn.Linear(input_dim, num_labels)
+
+        self.dropout_ops = nn.ModuleList(
+            [nn.Dropout(args.dropout_rate) for _ in range(self.args.dropout_num)]
+        )
+
+    def forward(self, x):
+        logits = None
+        for i, dropout_op in enumerate(self.dropout_ops):
+            if i == 0:
+                out = dropout_op(x)
+                logits = self.linear(out)
+
+            else:
+                temp_out = dropout_op(x)
+                temp_logits = self.linear(temp_out)
+                logits += temp_logits
+
+        # 相加还是求平均？
+        logits = logits / self.args.dropout_num
+        return self.linear(x)
